e2acd6f4f45b082b180b365d6b35b2c1dfbb8535
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-11-11 22:55:47 +0000

    feat: setup the dummy tests

---
+++ stryker.config.js
@@ -0,0 +1,21 @@
+// @ts-check
+/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+const config = {
+  _comment:
+    "This config was generated using 'stryker init'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.",
+  packageManager: 'npm',
+  reporters: ['html', 'clear-text', 'progress'],
+  testRunner: 'jest',
+  testRunner_comment:
+    'Take a look at https://stryker-mutator.io/docs/stryker-js/jest-runner for information about the jest plugin.',
+  coverageAnalysis: 'off',
+  jest: {
+    projectType: 'custom',
+    configFile: 'jest.config.js',
+    config: {
+      testEnvironment: 'node',
+    },
+    enableFindRelatedTests: true,
+  },
+};
+export default config;
